c2cb0d9a4b29aacd6476ab93ee8118ad48e3ada3
Mario Viapiano <mario.viapiano@wertfaktor.de>
2022-03-15 15:02:49 +0100

    extract BottleVerse

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -10,12 +10,25 @@ class Bottles
   end
 
   def verse(number)
+    BottleVerse.new(number).lyrics
+  end
+end
+
+class BottleVerse
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def lyrics
     bottle_number = BottleNumber.for(number)
     "#{bottle_number} of beer on the wall, ".capitalize +
       "#{bottle_number} of beer.\n" +
       "#{bottle_number.action}, " +
       "#{bottle_number.successor} of beer on the wall.\n"
   end
+  
 end
 
 class BottleNumber
